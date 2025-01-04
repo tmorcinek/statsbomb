091e708cb0ccc0e91ac5615aef5acdf7fe913950
tomasz-morcinek-dtiq <TMorcinek@dtiq.com>
2025-01-04 16:44:39 +0100

    Working pich visualization.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import json
+import pandas as pd
+from mplsoccer.pitch import Pitch
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open('../open-data/data/events/15946.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # Przekształć dane na DataFrame
+    df = pd.DataFrame(data)
+
+    # Rozpakuj typ wydarzenia (np. strzały, podania)
+    df['type_name'] = df['type'].apply(lambda x: x['name'] if isinstance(x, dict) else None)
+
+    # Filtrowanie wydarzeń do strzałów
+    shots = df[df['type_name'] == 'Shot']
+
+    # Wizualizacja: rysowanie boiska
+    pitch = Pitch(line_color='black', pitch_type='statsbomb')
+    fig, ax = pitch.draw(figsize=(10, 7))
+
+    # Dodanie strzałów do wizualizacji
+    for _, shot in shots.iterrows():
+        x, y = shot['location']  # Lokalizacja strzału
+        pitch.scatter(x, y, alpha=0.7, s=100, color='red', ax=ax)
+
+    # Wyświetlenie wizualizacji
+    ax.set_title('Lokalizacje Strzałów', fontsize=14)
+    fig.show()
+    plt.show()

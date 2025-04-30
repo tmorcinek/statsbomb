8097204cd026e8dc6e826db02c503218b12a6613
tomasz-morcinek-dtiq <TMorcinek@dtiq.com>
2025-04-30 10:57:00 +0200

    Getting match shots with displayed on pitch.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import matplotlib.pyplot as plt
-
-from utils import *
+import pandas as pd
+import utils as utl
+import match as m
 
 if __name__ == '__main__':
-    pd.set_option('display.width', 1000)
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
+    # pd.set_option('display.width', 1000)
+    # pd.set_option('display.max_rows', None)
+    # pd.set_option('display.max_columns', None)
 
     json = '../open-data/data/events/3938637.json'
-    # display_shots(json)
+    # utl.display_shots(json)
     # print_events(json)
-    # print_competitions()
-    # print_competition(55, 282)
-    print_match(3938637)
+    # utl.print_competitions()
+    # utl.print_competition(55, 282)
+    utl.print_match(3938637)
+    # m.print_match(3938637)
+    m.display_shots(json)
     # print_competition_bayer()
     # df = competition_df(9, 281)
     # print(df)
-    # plt.show()
+    plt.show()

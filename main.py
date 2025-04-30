8d8a9845f17d1cedbb0e0007c194324c43780f98
tomasz-morcinek-dtiq <TMorcinek@dtiq.com>
2025-04-30 12:52:49 +0200

    Testing of statsbombpy library.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
+from statsbombpy import sb
 import pandas as pd
 import utils as utl
 import match as m
 
-if __name__ == '__main__':
-    # pd.set_option('display.width', 1000)
-    # pd.set_option('display.max_rows', None)
-    # pd.set_option('display.max_columns', None)
 
+def display_shots():
     json = '../open-data/data/events/3938637.json'
     # utl.display_shots(json)
     # print_events(json)
@@ -20,3 +18,30 @@ if __name__ == '__main__':
     # df = competition_df(9, 281)
     # print(df)
     plt.show()
+
+
+def print_competitions():
+    competitions = sb.competitions()
+    filtered_competitions = competitions[(competitions['match_available_360'].notnull()) & (competitions['competition_international'] == True)]
+    print(filtered_competitions)
+
+
+def print_spanish_matches():
+    matches = sb.matches(55, 282)
+    team_name = 'Spain'
+    matches_spain = matches[(matches['home_team'] == team_name) | (matches['away_team'] == team_name)]
+    print(matches_spain)
+
+
+if __name__ == '__main__':
+    pd.set_option('display.width', 1000)
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+
+    # display_shots()
+    # print_competitions()
+
+    # print_spanish_matches()
+    lineups = sb.lineups(3943043)
+    # # players  = lineups[lineups['country'] == 'Spain']
+    print(lineups)
